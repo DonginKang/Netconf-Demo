faa15cd3d99c08e5ad2528bd169ddb000b0454f9
DonginKang <gang601@naver.com>
2017-02-13 15:15:06 +0900

    Update C4506.py

--- Netconf_Demo/Example/Hello/C4506.py
+++ Netconf_Demo/Example/Hello/C4506.py
@@ -9,12 +9,12 @@ import sys
 # the variables below assume the user is requesting access to a
 # Nexus device running in VIRL in the  DevNet Always On Sandbox
 # use the IP address or hostname of your Nexus device
-HOST = '192.168.0.78'
+HOST = ''
 # use the NETCONF port for your Nexus device
 PORT = 22
 # use the user credentials for your Nexus device
-USER = 'cisco'
-PASS = 'cisco'
+USER = ''
+PASS = ''
 
 
 # create a main() method
