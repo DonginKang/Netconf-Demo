ef0b5aa42d99771796d8dd78d2147adc7da55a11
DonginKang <gang601@naver.com>
2017-02-13 15:16:11 +0900

    Update C4506.py

--- Netconf_Demo/Example/Show_Config/C4506.py
+++ Netconf_Demo/Example/Show_Config/C4506.py
@@ -1,6 +1,6 @@
 from ncclient import manager
 
-with manager.connect(host="192.168.0.78", port=22, username="cisco", password = "cisco", hostkey_verify=False, device_params={'name':'default'}) as m:
+with manager.connect(host="", port=22, username="", password = "", hostkey_verify=False, device_params={'name':'default'}) as m:
     c = m.get_config(source = 'running')
     print type(c)
 #    with open("candidate.xml" , 'w') as f:
