4aa17a6dc390dddea3472cf1f0fec5eeb879d163
DonginKang <gang601@naver.com>
2017-02-13 15:16:43 +0900

    Update N9396.py

--- Netconf_Demo/Example/Show_Config/N9396.py
+++ Netconf_Demo/Example/Show_Config/N9396.py
@@ -1,5 +1,5 @@
 from ncclient import manager
 
-with manager.connect(host="192.168.0.34", port=22, username="admin", password = "cisco!2345", hostkey_verify=False, device_params={'name':'default'}) as m:
+with manager.connect(host="192.168.0.34", port=22, username="", password = "", hostkey_verify=False, device_params={'name':'default'}) as m:
     c = m.get(source = 'candidate')
     print c
